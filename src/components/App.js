0006eb4594d31f534ce388d7d68fc8d65d289330
Rémi <Remi.Tricotteaux@enseirb-matmeca.fr>
2021-05-28 21:43:53 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import Banner from './Banner';
+import Cart from './Cart';
+import ShoppingList from './ShoppingList'
+import Footer from './Footer'
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Cart />
+      <ShoppingList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
