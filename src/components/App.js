aa64a26475b804ee56ab2cbef9efbdb77023411f
rtricotteaux <rtricotteaux@gmail.com>
2021-05-28 22:50:36 +0200

    changement style

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,20 @@ import Banner from './Banner';
 import Cart from './Cart';
 import ShoppingList from './ShoppingList'
 import Footer from './Footer'
+import '../styles/Layout.css'
+import logo from '../assets/logo.png'
 
 function App() {
   return (
     <div>
-      <Banner />
-      <Cart />
-      <ShoppingList />
+      <Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+      <div className='lmj-layout-inner'>
+        <Cart />
+        <ShoppingList />
+      </div>
       <Footer />
     </div>
   );
