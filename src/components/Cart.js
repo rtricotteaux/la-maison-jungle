447908644d2325575e4b03c3cd415fc0878f23c8
rtricotteaux <rtricotteaux@gmail.com>
2021-05-28 22:40:48 +0200

    update du panier

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,28 @@
 import '../styles/Cart.css'
+import {useState} from 'react'
 
 function Cart() {
     const monsteraPrice = 8
-    const ivyPrice = 10
-    const flowerPrice = 15
-    return (
+    const [cart, updateCart] = useState(0)
+    const [isOpen, setIsOpen] = useState(true)
+
+    return isOpen ? (
         <div className='lmj-cart'>
+            <button onClick={() => setIsOpen(false)}>Fermer</button>
             <h2>Panier</h2>
-            <ul>
-            <li>Monstera : {monsteraPrice}€</li>
-            <li>Lierre : {ivyPrice}€</li>
-            <li>Fleurs : {flowerPrice}€</li>
-            </ul>
-            Total : {monsteraPrice + ivyPrice + flowerPrice }€
+            <div>
+                Monstera : {monsteraPrice}€
+                <button onClick={() => updateCart(cart+1)}>
+                    Ajouter
+                </button>
+            </div> 
+            <h3>Total : {monsteraPrice*cart}€</h3>
+            <button onClick={() =>updateCart(0)}>
+                Vider le Panier
+            </button>      
         </div>
+    ) : (
+        <button onClick={() => setIsOpen(true)}>Ouvrir le Panier</button>
     )
 }
 
